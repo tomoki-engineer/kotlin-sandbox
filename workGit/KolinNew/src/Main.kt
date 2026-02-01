e345eb3954b33defa824d9fb2eab0ed66db7f490
Tomoki.M <tomoki.build@gmail.com>
2026-02-01 17:07:38 +0900

    Kotlin応用　コレクション変換と分類の練習

---
+++ workGit/KolinNew/src/Main.kt
@@ -0,0 +1,28 @@
+data class User(val name:String, val age: Int)
+
+val users = listOf(
+    User("Vader", 31),
+    User("Luke", 19),
+    User("Leia", 19),
+    User("Han", 35)
+)
+
+fun main() {
+    /* 名前の頭文字をキーにしてUserをマップ化 */
+    val mapByFirst = users.associateBy { it.name.first() }
+    println(mapByFirst)
+
+    /* ネストされたリストを平らにする*/
+    val nestedList = listOf(
+        listOf(1, 2),
+        listOf(3, 4)
+    )
+
+    val flat = nestedList.flatMap {it}
+    println(flat)
+
+    /* 条件で二分割*/
+    val (adults, minors) = users.partition {it.age >= 20}
+    println("大人：$adults")
+    println("未成年：$minors")
+}
