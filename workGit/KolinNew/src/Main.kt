1fc197ec7ef209221936dc70c1e516be435c1efd
Tomoki.M <tomoki.build@gmail.com>
2026-02-04 23:35:35 +0900

    Add User Sort

--- workGit/KolinNew/src/Main.kt
+++ workGit/KolinNew/src/Main.kt
@@ -19,26 +19,49 @@ fun main() {
         listOf(3, 4)
     )
 
-    val flat = nestedList.flatMap {it}
+    val flat = nestedList.flatMap { it }
     println(flat)
-
-    /* 条件で二分割*/
-    val (adults, minors) = users.partition {it.age >= 20}
-    println("大人：$adults")
-    println("未成年：$minors")
 
     /*総人数 */
     val totalPeopleCount = users.size
     println("総人数：$totalPeopleCount")
 
     /*平均年齢 */
-    val averageAge = users.map{it.age}.average()
+    val averageAge = users.map { it.age }.average()
     println("平均年齢：$averageAge")
 
     /*最年長 */
-    val oldestPerson = users.maxByOrNull{it.age}
+    val oldestPerson = users.maxByOrNull { it.age }
     println("最年長：${oldestPerson?.name} (${oldestPerson?.age})")
     /*未成年カウント */
-    val minorCount = users.count{ it.age < 20 }
+    val minorCount = users.count { it.age < 20 }
     println("未成年人数：$minorCount")
+
+    /* 年齢順に並べて表示 */
+    users.sortedBy{it.age}.forEach{println("${it.age}:${it.name}")}
+
+    /*　年齢ごとにグループ化して表示 */
+    users.sortedBy { it.age }.groupBy { it.age }
+        .forEach { (age, group) ->
+            val names = group.joinToString(", ") { it.name }
+            println("${age}歳: $names") }
+
+    /*　年齢ごとの人数も表示する */
+    users.sortedBy { it.age }.groupBy { it.age }
+        .forEach { (age, group) ->
+            val names = group.joinToString(", ") { it.name }
+            println("${age}歳 (${group.size}人): $names") }
+
+
+    //大人・未成年ごとに、年齢順で表示
+    val (adults, minors) = users.partition {it.age >= 20}
+    println("【大人】")
+    adults.sortedBy { it.age }.forEach {
+        println("${it.age}歳: ${it.name}")
+    }
+
+    println("【未成年】")
+    minors.sortedBy { it.age }.forEach {
+        println("${it.age}歳: ${it.name}")
+    }
 }
