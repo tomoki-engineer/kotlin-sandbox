20bbb91e1e50ff4aa248f2483f004d4d341d806e
Tomoki.M <tomoki.build@gmail.com>
2026-02-07 23:17:06 +0900

    Add mapNameList

--- workGit/KolinNew/src/Main.kt
+++ workGit/KolinNew/src/Main.kt
@@ -1,7 +1,7 @@
 import model.User
 
 val users = listOf(
-    User("Vader", 31),
+    User("Vader", 31)：
     User("Luke", 19),
     User("Leia", 19),
     User("Han", 35)
@@ -9,18 +9,12 @@ val users = listOf(
 
 fun main() {
 
-    //名前 → 未成年かどうか(Boolean)のMap
-    val nameToMinors = users.associate{it.name to (it.age<20)}
-    println(nameToMinors)
+    // 大人/未成年別 Map<User>
+    val adultsAndMinorsMap: Map<Boolean,List<User>> = users.groupBy { it.age >= 20 }
+    println(adultsAndMinorsMap)
 
-    //大人の名前だけを、年齢順のListで作る
-    val adultsNameList = mutableListOf<String>()
-
-    users.filter { it.age >= 20 }
-        .sortedBy { it.age }.forEach {
-            user -> adultsNameList.add(user.name)
-        }
-
-    println(adultsNameList)
-
+    // 大人／未成年別 Map、年齢順に名前リスト
+    val adultsAndMinorsNames: Map<Boolean,List<String>> = users
+        .groupBy{it.age>=20}
+        .mapValues { entry -> entry.value.sortedBy { it.age }.map{it.name} }
 }
--- workGit/KolinNew/src/service/UserService.kt
+++ workGit/KolinNew/src/service/UserService.kt
@@ -45,6 +45,20 @@ object UserService {
         val ageToNameListMap = users
             .groupBy { it.age }.toSortedMap().mapValues { it.value.joinToString(", ") {it.name} }
         println(ageToNameListMap)
+
+        //名前 → 未成年かどうか(Boolean)のMap
+        val nameToMinors = users.associate{it.name to (it.age<20)}
+        println(nameToMinors)
+
+        //大人の名前だけを、年齢順のListで作る
+        val adultsNameList = mutableListOf<String>()
+
+        users.filter { it.age >= 20 }
+            .sortedBy { it.age }.forEach {
+                    user -> adultsNameList.add(user.name)
+            }
+
+        println(adultsNameList)
     }
 
     /*未使用ファンクション */
