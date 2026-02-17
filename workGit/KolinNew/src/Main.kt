f65ee5dcc699d09da2df3623787f26bc77a0a2ae
Tomoki.M <tomoki.build@gmail.com>
2026-02-17 22:28:05 +0900

    Add highSalaryByDepartment

--- workGit/KolinNew/src/Main.kt
+++ workGit/KolinNew/src/Main.kt
@@ -2,6 +2,7 @@ import model.DepartmentStats
 import model.Employee
 import model.PublicEmployee
 import model.User
+import kotlin.collections.sortedBy
 
 val users = listOf(
     User("Vader", 31),
@@ -11,31 +12,34 @@ val users = listOf(
 )
 
 val employees = listOf(
-    Employee("A", "Dev", 500, 31),
-    Employee("B", "Dev", 600, 48),
-    Employee("C", "HR", 400, 19),
-    Employee("D", "HR", 450,28),
-    Employee("E", "Dev", 700, 56),
+    Employee("Vader", "IT", 900, 31),
+    Employee("Luke", "IT", 500, 19),
+    Employee("Kenobi", "IT", 800, 45),
+
+    Employee("Leia", "HR", 600, 19),
+    Employee("Han", "HR", 750, 35),
+    Employee("Rey", "HR", 720, 22),
+
+    Employee("Yoda", "Finance", 1200, 900),
+    Employee("Finn", "Finance", 650, 23)
+)
+
+val publicEmployees = listOf(
+    PublicEmployee("Vader", 31, 900),
+    PublicEmployee("Luke", null, 500),
+    PublicEmployee("Leia", 19, 600),
+    PublicEmployee("Han", null, 700),
+    PublicEmployee("Kenobi", 45, 800)
 )
 
 fun main() {
-    val employees = listOf(
-        PublicEmployee("Vader", 31, 900),
-        PublicEmployee("Luke", null, 500),
-        PublicEmployee("Leia", 19, 600),
-        PublicEmployee("Han", null, 700)
-    )
+    val highSalaryByDepartment = employees
+        .groupBy{it.department}
+        .mapValues { (_, list) ->
+            list.filter { it.salary >= 700 }
+                .sortedByDescending { it.salary }
+                .map { it.name }
+        }
 
-
-
-    /* 年齢がnullじゃない*/
-    val ageList = employees.mapNotNull{it.age}
-    println(ageList)
-
-    /* 年齢がnullじゃない人の名前だけ*/
-    val nameList = employees.mapNotNull{
-        emp -> emp.age?.let{emp.name}
-    }
-
-    println(nameList)
+    println(highSalaryByDepartment)
 }
